4372473009b0a546be2688b5a83ad13988469176
Diego Guerrero Alexander <diegoguerreroalexander@gmail.com>
2020-09-08 11:12:48 -0500

    CRUD ENV ready

---
+++ tabyapp/controllers/habits.js
@@ -0,0 +1,3 @@
+const User = require("../models/User")
+const Habit = require("../models/Habit")
+
---
+++ tabyapp/routes/habits.js
@@ -0,0 +1,8 @@
+const express = require("express")
+const router = express.Router()
+const {
+
+} = require("../controllers/habits")
+
+
+module.exports = router
